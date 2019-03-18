d3fcaf9af9f0ba601477444fb81d8486a1fdbc18
Ohaya Onyebuchi Michael <onyebuchi_ohaya@yahoo.co.uk>
2019-03-18 15:17:10 +0100

    changed my database from sqlite to postgresql

--- vagrant/catalog/__init__.py
+++ vagrant/catalog/__init__.py
@@ -6,7 +6,7 @@ from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://catalog:catalog@localhost/site.db"
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 bcrypt = Bcrypt(app)
